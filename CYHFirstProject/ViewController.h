374753dfe971f3f61bc3acef9afee7cae08cb2f4
cyh <>
2019-05-20 19:26:50 +0800

    first commit

---
+++ CYHFirstProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CYHFirstProject
+//
+//  Created by cyh on 2019/5/20.
+//  Copyright © 2019 cyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CYHFirstProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CYHFirstProject
+//
+//  Created by cyh on 2019/5/20.
+//  Copyright © 2019 cyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
